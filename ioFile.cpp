5c294a827947643b4569723e3b08fd4ac2ad5f3b
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-09 17:45:53 +0700

    menutup file 'infile'

--- ioFile.cpp
+++ ioFile.cpp
@@ -45,5 +45,7 @@ int main()
             // dan tampilkan di sini
             cout << baris << '\n';   
         }
+        // tutup file tersebut setelah selesai
+        infile.close();
     }
 }
