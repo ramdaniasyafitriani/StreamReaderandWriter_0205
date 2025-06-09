04b54f922fc1df6b99e5ffae0b211155560639ed
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-09 17:43:55 +0700

    melakukan perulangan while di setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -39,6 +39,10 @@ int main()
     // jika file ada, maka
     if (infile.is_open())
     {
+        // melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
 
+        }
     }
 }
