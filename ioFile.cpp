83a163e93a409367b4f62fbf70af4e0f8631beb8
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-06-09 17:44:28 +0700

    tampilkan

--- ioFile.cpp
+++ ioFile.cpp
@@ -42,7 +42,8 @@ int main()
         // melakukan perulangan setiap baris 
         while (getline(infile, baris))
         {
-
+            // dan tampilkan di sini
+            cout << baris << '\n';   
         }
     }
 }
